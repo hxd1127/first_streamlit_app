445358a449c3ef4767e83c238ea479d3db7db51b
hxd1127 <102530864+hxd1127@users.noreply.github.com>
2022-04-14 15:01:32 +0530

    Update streamlit_app.py
    json view

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,7 @@ fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header('fruitvise fruit Advice'
 import requests
 fruitvise_response =requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text =fruitvise_response
+streamlit.text (fruitvise_response.json)

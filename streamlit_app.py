7c14c3a083cc5f0a59d09ad83385dd2a84807db4
hxd1127 <102530864+hxd1127@users.noreply.github.com>
2022-04-14 15:05:49 +0530

    Update streamlit_app.py
    comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.i
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header('fruitvise fruit Advice')
+#streamlit.header('fruitvise fruit Advice')
 
-import requests
-fruitvise_response =requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text (fruitvise_response.json())
+#import requests
+#fruitvise_response =requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text (fruitvise_response.json())
